ea61f73d50136e778d984b3bd46559793ad73c9f
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-08-27 12:44:24 -0300

    Added inifite scrolling

--- src/components/CharacterList/CharacterList.js
+++ src/components/CharacterList/CharacterList.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import Character from "../Character/Character";
 import { CharactersListContainer } from "./CharacterListStyles";
 
 const CharacterList = ({ filteredCharacters, setClicked, setDetail }) => {
+  const [currentPage, setCurrentPage] = useState(20);
+  let items = filteredCharacters.slice(0, currentPage);
+
+  window.addEventListener("scroll", () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (clientHeight + scrollTop >= scrollHeight - 1) {
+      console.log(items);
+      setCurrentPage(currentPage + 5);
+    }
+  });
+
   return (
     <CharactersListContainer>
-      {filteredCharacters.length > 0 ? (
-        filteredCharacters.map((character) => {
+      {items.length > 0 ? (
+        items.map((character) => {
           return (
             <>
               <Character
